449ec20f0a6d27dde1c3a3f531ae5f2c8555112a
GeorgeHarris86 <geoharr21@gmail.com>
2023-09-22 23:19:52 -0400

    finished part 1

---
+++ Method exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace MethodExerCise;
+
+
+    internal class Program
+    {
+   
+        private static void Main(string[] args)   
+        {
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string Color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string Animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite song?");
+            string Song = Console.ReadLine();
+
+
+            Console.WriteLine($"There was guy called (userName), he loved the color (Color) he loved it so much he had to have it on all the things he owned.");
+            Console.WriteLine($"(userName) loved the color (Color) so much he had his pet (Animal) painted to match his favorite color.");
+            Console.WriteLine($"(userName) had a song made I'm (Song) it was a smash hit even his pet (Animal) danced to it");
+        }
+    }
